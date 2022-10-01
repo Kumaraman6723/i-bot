1828e243b83e827d5812710a46830282ac92ab7b
Kumaraman6723 <113774604+Kumaraman6723@users.noreply.github.com>
2022-10-01 13:38:00 +0530

    Update dbConnection.js

--- Db/dbConnection.js
+++ Db/dbConnection.js
@@ -2,8 +2,8 @@ require("dotenv").config({ path: "./Keys.env" });
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const mdbUsername = process.env.MDB_USERNAME;
-const mdbPassword = process.env.MDB_PASSWORD;
+const mdbUsername = process.env.amanjadoo;
+const mdbPassword = process.env.Pubg god1;
 const uri = `mongodb+srv://${mdbUsername}:${mdbPassword}@cluster0.br8pm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri);
 
