83f8673426d46f17f89bbc96578039b14d91ce8b
Kumaraman6723 <113774604+Kumaraman6723@users.noreply.github.com>
2022-10-01 13:32:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,9 +20,8 @@ const Helper = require("./bot_modules/helper.js");
 const Sticker = require("./bot_modules/sticker.js");
 const InstaDownloader = require("./bot_modules/instaDownloader.js");
 const Crypto = require("./bot_modules/crypto.js");
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://amanjadoo:<password>@cluster0.limbxex.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const mdClient = require("./Db/dbConnection.js");
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
